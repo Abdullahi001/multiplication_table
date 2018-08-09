d4334fe0a8aa212c56f1961c121568f49710bd69
Abdullahi Musa <abdullahimusa4991@gmail.com>
2018-08-09 08:25:02 +0300

    structuring html page

---
+++ app.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <title>Multiplication Table</title>
+  <script src="app.js">
+
+  </script>
+</head>
+<body>
+  <hr>
+  <footer>
+  <p>Copyright Abdullahi Musa <span id="footer-date">2018</span></p>
+  </footer>
+</body>
+</html>
